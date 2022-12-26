f8e421d2dcc6b942b6c8e39ff7eec4c95e9c4b07
Mihail <quickautotrans@gmail.com>
2022-12-26 11:07:51 +0300

    1) Не выбрасывай исключения. Просто распечатай. 2) Каждый метод дао должен вызывать Util.getConnection(), работать с Connection и закрывать его. 3) Добавь логику транзакций.
    исправил

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,14 +3,13 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+
+    private static Connection connection = Util.getConnection();
     public UserDaoJDBCImpl() {
 
     }
@@ -19,34 +18,54 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void createUsersTable() {
 
-        try(Statement statement = Util.getConnection().createStatement()) {
+
+
+        try {
+            Statement statement = connection.createStatement();
 
             statement.executeUpdate("CREATE TABLE `mydbtest`.`users` (`id` INT NOT NULL AUTO_INCREMENT, " +
                     "`name` VARCHAR(45) NOT NULL,`lastname` VARCHAR(45) NOT NULL," +
                     "`age` INT NOT NULL, PRIMARY KEY (`id`)) " +
                     "ENGINE = InnoDB DEFAULT CHARACTER SET = utf8");
 
-
+            connection.commit();
+            connection.close();
 
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
         }
 
 
     }
 
     public void dropUsersTable() {
-        try(Statement statement = Util.getConnection().createStatement()) {
+        try {
+            Statement statement = connection.createStatement();
             statement.executeUpdate("DROP TABLE IF EXISTS users");
+
+            connection.commit();
+            connection.close();
+
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
 
-        try(PreparedStatement preparedStatement =
-                    Util.getConnection().prepareStatement("INSERT INTO users(name,lastname,age) VALUES (?, ?, ?)")) {
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("INSERT INTO users(name,lastname,age) VALUES (?, ?, ?)");
             preparedStatement.setString(1, name);
             preparedStatement.setString(2,lastName);
             preparedStatement.setInt(3, age);
@@ -54,21 +73,38 @@ public class UserDaoJDBCImpl implements UserDao {
             preparedStatement.executeUpdate();
             System.out.println("User с именем – " + name + " добавлен в базу данных");
 
+            connection.commit();
+            connection.close();
+
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
         }
 
     }
 
     public void removeUserById(long id) {
-        try(PreparedStatement preparedStatement =
-                    Util.getConnection().prepareStatement("DELETE FROM users WHERE id = ?");) {
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("DELETE FROM users WHERE id = ?");
 
             preparedStatement.setLong(1, id);
             preparedStatement.executeUpdate();
 
+            connection.commit();
+            connection.close();
+
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
         }
 
     }
@@ -78,7 +114,8 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
-        try(Statement statement = Util.getConnection().createStatement()) {
+        try {
+            Statement statement = connection.createStatement();
 
             ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
 
@@ -90,9 +127,17 @@ public class UserDaoJDBCImpl implements UserDao {
                 user.setAge(resultSet.getByte("age"));
 
                 users.add(user);
+
+                connection.commit();
+                connection.close();
             }
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            try {
+                Util.getConnection().rollback();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
         }
         System.out.println(users.toString());
         return users;
@@ -100,12 +145,21 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void cleanUsersTable() {
 
-        try(Statement statement = Util.getConnection().createStatement()) {
+        try {
+            Statement statement = connection.createStatement();
 
             statement.executeUpdate("DELETE FROM users");
 
+            connection.commit();
+            connection.close();
+
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            try {
+                Util.getConnection().rollback();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
         }
 
     }
